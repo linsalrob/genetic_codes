56cb098b38272580dc1da03bc84fd3791d098e10
linsalrob <raedwards@gmail.com>
2023-12-22 08:33:34 +1030

    Adding accessory functions to make easier

--- pygenetic_code/code_to_table.py
+++ pygenetic_code/code_to_table.py
@@ -3,26 +3,35 @@ Convert a genetic code to a json table
 """
 import sys
 
-from .Error import InvalidTranslationTable
-from .genetic_code import genetic_codes
+from .Error import InvalidTranslationTableException
+from .genetic_code import genetic_codes, all_possible_codons
+
 
 def code_to_table(translation_table):
     """
-    Return the json object for this code
+    Return a dictionary for this genetic code. The dictionary has two entries, 'initiators' which are the start
+    codons, and 'codons' that are the genetic code.
+
     :param genetic_code: the genetic code (either as an int or a string)
     :return: the json object
     """
-
 
     code = genetic_codes()
     if (str(translation_table)) in code:
         return code[str(translation_table)]
     else:
-        raise InvalidTranslationTable(f"Translation table {translation_table} is not defined")
+        raise InvalidTranslationTableException(f"Translation table {translation_table} is not defined")
 
-def codons():
+
+def codons_to_translation_tables():
+    """
+    Get a dictionary of all the codons, with the values being a dictionary of translation tables and their encodings
+    :return: a dictionary of dictionaries
     """
 
-    :return:
-    """
-
+    code = genetic_codes()
+    codons = {x:{} for x in all_possible_codons()}
+    for i in code:
+        for cd in code[i]['codons']:
+            codons[cd][i] = code[i]['codons'][cd]
+    return codons
